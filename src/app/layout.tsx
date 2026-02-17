699af6660174b931bd99b33824e40ea6f427783a
Pavel Kocman <kocmanpavel@gmail.com>
2026-02-17 19:37:00 +0100

    feat: :sparkles: setup shadcn and basic layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
-        {children}
+        <div className="min-h-screen">{children}</div>
       </body>
     </html>
   );
